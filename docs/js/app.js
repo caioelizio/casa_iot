f553835844c2183d416fcab86a00486b794e5388
Caio Elizio <caioelizio@gmail.com>
2021-03-24 22:13:39 -0300

    Update app.js

--- docs/js/app.js
+++ docs/js/app.js
@@ -3,7 +3,7 @@
 var config = {
   apiKey: "AIzaSyBj6zwBDJWLWEr3pVNCu8FkT55tKNLd1_0",
   authDomain: "casa1014-48a5a.firebaseapp.com",
-  databaseURL: "https://casa1014-48a5a.firebaseio.com/",
+  databaseURL: "https://casa1014-48a5a.firebaseio.com",
   storageBucket: "casa1014-48a5a.appspot.com"
 };
 
