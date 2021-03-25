10f8bf5ed5342c7b1f56be623e04cad967ab7157
Caio Elizio <caioelizio@gmail.com>
2021-03-24 21:14:44 -0300

    Update app.js

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,10 +1,10 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var config = {
-  apiKey: "AIzaSyAx9iNfHHfaUhgXMweYP6wLxurFSZIsDQo",
-  authDomain: "industria-iot.firebaseapp.com",
-  databaseURL: "https://industria-iot-default-rtdb.firebaseio.com",
-  storageBucket: "industria-iot.appspot.com"
+  apiKey: "AIzaSyBj6zwBDJWLWEr3pVNCu8FkT55tKNLd1_0",
+  authDomain: "casa1014-48a5a.firebaseapp.com",
+  databaseURL: "https://casa1014-48a5a.firebaseio.com/",
+  storageBucket: "casa1014-48a5a.appspot.com"
 };
 
 firebase.initializeApp(config);
