e1d1e1f00d3aadcc24a8981993bf2cd68032b3fc
Caio Elizio <caioelizio@gmail.com>
2021-04-19 20:34:43 -0300

    Update and rename real-time-database.js to real-time-database-DHT11-serv.js

---
+++ docs/js/real-time-database-DHT11-serv.js
@@ -0,0 +1,39 @@
+var watch = (() => {
+  const tempInput   = document.querySelector("#tempInput");
+
+  const create = (temp) => {
+    const data    = {
+      temp
+    }
+
+    return firebase.database().ref().child('temp').push(data);
+  };
+
+  const resetForm = () => {
+    tempInput.value   = '';
+  }
+
+  const userListView = (snapshot) => {
+    userList.innerHTML = '';
+    snapshot.forEach(item => {
+      let li = document.createElement('li');
+      li.appendChild(document.createTextNode(item.val().temp + ': ' + item.val().age));
+      userList.appendChild(li);
+    });
+  }
+
+  return {
+    init: () => {
+      firebase.database().ref('temp').on('temp', userListView);
+      
+      addButton.addEventListener('click', (event) => {
+        event.preventDefault();
+        create(tempInput.value, ageInput.value)
+        resetForm();
+      });
+    }
+  }
+  
+})();
+
+watch.init();
